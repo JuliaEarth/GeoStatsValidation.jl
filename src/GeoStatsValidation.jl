028c368500a67644e698bb75595a3540f41c1d59
Elias Carvalho <eliascarvdev@gmail.com>
2023-12-18 16:28:07 -0300

    Add 'defaultloss' helper function

--- src/GeoStatsValidation.jl
+++ src/GeoStatsValidation.jl
@@ -1,8 +1,13 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
 module GeoStatsValidation
 
 using Meshes
 using GeoTables
 using Transducers
+using DataScienceTraits
 using DensityRatioEstimation
 
 using GeoStatsModels: GeoStatsModel
@@ -11,11 +16,14 @@ using StatsLearnModels: Learn, input, output
 using GeoStatsTransforms: Interpolate, InterpolateNeighbors
 
 using ColumnSelectors: selector
+using GeoStatsBase: weight, folds, mean
 using GeoStatsBase: WeightingMethod, DensityRatioWeighting, UniformWeighting
 using GeoStatsBase: FoldingMethod, BallFolding, BlockFolding, OneFolding, UniformFolding
-using GeoStatsBase: weight, folds, defaultloss, mean
+using DataScienceTraits: Continuous, Categorical
+using LossFunctions: L2DistLoss, MisclassLoss
 using LossFunctions.Traits: SupervisedLoss
 
+include("utils.jl")
 include("cverror.jl")
 
 export cverror, LeaveOneOut, LeaveBallOut, KFoldValidation, BlockValidation, WeightedValidation, DensityRatioValidation
